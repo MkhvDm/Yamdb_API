50f8b28ef22114694d63444a103097d4fce4bf89
Dmitriy Mikheev <mkhvdm@gmail.com>
2022-08-11 22:27:18 +0300

    add load db from csv

--- api_yamdb/reviews/management/commands/load_db.py
+++ api_yamdb/reviews/management/commands/load_db.py
@@ -3,7 +3,6 @@ import csv
 from reviews import models
 from users.models import User
 
-
 models_assignments = {
     'users': User,
     'category': models.Category,
@@ -21,37 +20,27 @@ models_assignments = {
 # value - tuple of variants of names in csv table's columns
 fk_names = {
     'category': ('category_id', 'category'),
-    'titles': ('title_id', 'titles', 'title'),
-    'genre': ('genre_id', 'genres', 'genre'),
     'users': ('author', 'users', 'author_id', 'user_id', ),
-    'review': ('review_id', 'review', 'reviews'),
 }
 
 
 class Command(BaseCommand):
-    help = 'The Zen of Python'
+    help = 'Load data in database (SQLite3) from CSV-file.'
 
     def handle(self, *args, **options):
-        tables = list(models_assignments.keys())
-        for table in tables:
+        table_names = list(models_assignments.keys())
+        for table in table_names:
             csv_file = f'static/data/{table}.csv'
             with open(csv_file, 'r', encoding='utf-8') as csv_file:
                 csv_reader = csv.DictReader(csv_file, delimiter=',')
                 for row in csv_reader:
-                    print(row)
                     model = models_assignments.get(table)
-                    print('MODEL:', model)
-
-                    # foreign key fields update:
-                    for key in row.keys():
+                    for key in row.keys():  # foreign key fields update
                         for item in fk_names.items():
-                            print('item:', item)
-                            print('item[1]:', item[1])
                             if key in item[1]:
                                 fk_model = models_assignments[item[0]]
                                 fk_inst = fk_model.objects.get(pk=int(row[key]))
                                 row[key] = fk_inst
-                                print('New row:', row)
-
-                    inst = model.objects.update_or_create(**row)
-        print('END')
+                    model.objects.update_or_create(**row)
+            print(f'Loading "{table}" done!')
+        print('Load complete!')
